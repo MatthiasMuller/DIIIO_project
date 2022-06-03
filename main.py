d0e43d0f46841a151c82a90194fdf0881a1fdfdf
Matthias Muller <matthias.wilhelm.muller@gmail.com>
2022-06-02 21:02:12 -0400

    Removed if main.

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+from gurobipy import Model, GRB
+
 from dispatching.managers import VehicleManager, OrderManager
 
 # MAIN
 
-if __name__ == '__main__':
-    # VehicleManager
-    veh_m = VehicleManager()
-    veh = veh_m.create_random()
-    print(f"Vehicle: {veh}")
+# VehicleManager
+veh_m = VehicleManager()
+veh = veh_m.create_random()
+print(f"Vehicle: {veh}")
 
-    # OrderManager
-    ord_m = OrderManager()
-    orders = [ord_m.create_random() for _ in range(10)]
-    for i in orders:
-        print(f"Order: {i}")
+# OrderManager
+ord_m = OrderManager()
+orders = [ord_m.create_random() for _ in range(10)]
+for i in orders:
+    print(f"Order: {i}")
+
+# plotter = Plotter()
+# plotter.plot(orders=orders, center=CENTER)
 
 
-
-
-
-    # plotter = Plotter()
-    # plotter.plot(orders=orders, center=CENTER)
+mdl = Model()
